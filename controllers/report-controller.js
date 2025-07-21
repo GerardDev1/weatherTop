9a64bf5a8650d15675a82c6c46c08d149931f17c
Patrick Fox <foxpatrick90@gmail.com>
2025-07-21 17:53:09 +0100

    New commit

---
+++ controllers/report-controller.js
@@ -0,0 +1,31 @@
+import { stationStore } from "../models/station-store.js";
+import { reportStore } from "../models/report-store.js";
+
+export const reportController = {
+  async index(request, response) {
+    const stationId = request.params.stationid;
+    const reportId = request.params.reportid;
+    console.log(`Editing Report ${reportId} from Station ${stationId}`);
+    const viewData = {
+      title: "Edit Report",
+      station: await stationStore.getStationById(stationId),
+      report: await reportStore.getReportById(reportId),
+    };
+    response.render("report-view", viewData);
+  },
+
+  async update(request, response) {
+    const stationId = request.params.stationid;
+    const reportId = request.params.reportid;
+    const updatedReport = {
+      code: request.body.code,
+      windspeed: request.body.windspeed,
+      pressure: request.body.pressure,
+      winddirection: request.body.winddirection,
+    };
+    console.log(`Updating Report ${reportId} from Station ${stationId}`);
+    const report = await reportStore.getReportById(reportId);
+    await reportStore.updateReport(report, updatedReport);
+    response.redirect("/station/" + stationId);
+  },
+};
---
+++ controllers/station-controller.js
@@ -0,0 +1,36 @@
+import { stationStore } from "../models/station-store.js";
+import { reportStore } from "../models/report-store.js";
+
+export const stationController = {
+
+  async index(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const viewData = {
+      title: "Station",
+      station: station,
+    };
+    response.render("station-view", viewData);
+  },
+
+  async addReport(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const newReport = {
+      code: request.body.code,
+      temp: request.body.temp,
+      windspeed: request.body.windspeed,
+      winddirection: request.body.winddirection,
+      pressure: request.body.pressure,
+    };
+    console.log(`adding report ${newReport.code}`);
+    await reportStore.addReport(station._id, newReport);
+    response.redirect("/station/" + station._id);
+  },
+
+  async deleteReport(request, response) {
+    const stationId = request.params.stationid;
+    const reportId = request.params.reportid;
+    console.log(`Deleting Report ${reportId} from Station ${stationId}`);
+    await reportStore.deleteReport(request.params.reportId);
+    response.redirect("/station/" + stationId);
+  },
+};
